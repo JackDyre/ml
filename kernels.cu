361ab5f134c69fb0482c4c07ad8434cc7f252ed1
Jack Dyre <83248599+JackDyre@users.noreply.github.com>
2025-02-23 12:25:56 -0600

    new matrix impl

--- kernels.cu
+++ kernels.cu
@@ -6,8 +6,15 @@
 
 #define ptr_idx(stride, row, col) ((row) * stride + (col))
 
+const dim3 THREADS_PER_BLOCK = dim3(16, 16, 1);
+dim3 blocks_per_grid(dim3 inp_dim) {
+  return dim3((inp_dim.x + THREADS_PER_BLOCK.x - 1) / THREADS_PER_BLOCK.x,
+              (inp_dim.y + THREADS_PER_BLOCK.y - 1) / THREADS_PER_BLOCK.y,
+              (inp_dim.z + THREADS_PER_BLOCK.z - 1) / THREADS_PER_BLOCK.z);
+}
+
 template <typename T>
-__global__ void matrix_fill_kernel(T *dst, T val, size_t rows, size_t cols) {
+__global__ void _matrix_fill_kernel(T *dst, T val, size_t rows, size_t cols) {
   assert(threadIdx.z == 0 && blockIdx.z == 0);
 
   size_t row = blockIdx.x * blockDim.x + threadIdx.x;
@@ -19,8 +26,15 @@ __global__ void matrix_fill_kernel(T *dst, T val, size_t rows, size_t cols) {
 }
 
 template <typename T>
-__global__ void matrix_fill_rand_kernel(T *dst, size_t rows, size_t cols, T low,
-                                        T high, unsigned seed) {
+void launch_matrix_fill_kernel(T *dst, T val, size_t rows, size_t cols) {
+  _matrix_fill_kernel<<<blocks_per_grid(dim3(rows, cols, 1)),
+                        THREADS_PER_BLOCK>>>(dst, val, rows, cols);
+  assert(cudaSuccess == cudaDeviceSynchronize());
+}
+
+template <typename T>
+__global__ void _matrix_fill_rand_kernel(T *dst, size_t rows, size_t cols,
+                                         T low, T high, unsigned seed) {
   assert(threadIdx.z == 0 && blockIdx.z == 0);
 
   size_t row = blockIdx.x * blockDim.x + threadIdx.x;
@@ -34,7 +48,16 @@ __global__ void matrix_fill_rand_kernel(T *dst, size_t rows, size_t cols, T low,
 }
 
 template <typename T>
-__global__ void matrix_add_kernel(T *dst, T *other, size_t rows, size_t cols) {
+void launch_matrix_fill_rand_kernel(T *dst, size_t rows, size_t cols, T low,
+                                    T high, unsigned seed) {
+  _matrix_fill_rand_kernel<<<blocks_per_grid(dim3(rows, cols, 1)),
+                             THREADS_PER_BLOCK>>>(dst, rows, cols, low, high,
+                                                  seed);
+  assert(cudaSuccess == cudaDeviceSynchronize());
+}
+
+template <typename T>
+__global__ void _matrix_add_kernel(T *dst, T *other, size_t rows, size_t cols) {
   assert(threadIdx.z == 0 && blockIdx.z == 0);
 
   size_t row = blockIdx.x * blockDim.x + threadIdx.x;
@@ -46,8 +69,15 @@ __global__ void matrix_add_kernel(T *dst, T *other, size_t rows, size_t cols) {
 }
 
 template <typename T>
-__global__ void matrix_mul_kernel(T *dst, T *a, T *b, size_t rows, size_t cols,
-                                  size_t inner_size) {
+void launch_matrix_add_kernel(T *dst, T *other, size_t rows, size_t cols) {
+  _matrix_add_kernel<<<blocks_per_grid(dim3(rows, cols, 1)),
+                       THREADS_PER_BLOCK>>>(dst, other, rows, cols);
+  assert(cudaSuccess == cudaDeviceSynchronize());
+}
+
+template <typename T>
+__global__ void _matrix_mul_kernel(T *dst, T *a, T *b, size_t rows, size_t cols,
+                                   size_t inner_size) {
 
   assert(threadIdx.z == 0 && blockIdx.z == 0);
 
@@ -61,6 +91,14 @@ __global__ void matrix_mul_kernel(T *dst, T *a, T *b, size_t rows, size_t cols,
     }
     dst[ptr_idx(cols, row, col)] = sum;
   }
+}
+
+template <typename T>
+void launch_matrix_mul_kernel(T *dst, T *a, T *b, size_t rows, size_t cols,
+                              size_t inner_size) {
+  _matrix_mul_kernel<<<blocks_per_grid(dim3(rows, cols, 1)),
+                       THREADS_PER_BLOCK>>>(dst, a, b, rows, cols, inner_size);
+  assert(cudaSuccess == cudaDeviceSynchronize());
 }
 
 #endif // KERNELS_CU
--- matrix.cu
+++ matrix.cu
@@ -1,5 +1,8 @@
+#include "kernels.cu"
 #include "lazy_alloc.cu"
 #include "types.h"
+#include <iomanip>
+#include <iostream>
 
 #ifndef MATRIX_CU
 #define MATRIX_CU
@@ -43,19 +46,109 @@ public:
                              shape.rows * shape.cols * sizeof(float)));
   }
 
+  void host_print() {
+    float *ptr = (float *)allocator.get_host_ptr();
+
+    std::cout << std::fixed << std::setprecision(5);
+    std::cout << "[" << std::endl;
+    for (size_t r = 0; r < shape.rows; r++) {
+      std::cout << "    ";
+      for (size_t c = 0; c < shape.cols; c++) {
+        std::cout << std::setw(10) << ptr[ptr_idx(shape.cols, r, c)] << " ";
+      }
+      std::cout << std::endl;
+    }
+    std::cout << "]" << std::endl;
+  }
+
   void host_fill(float val) {
     float *ptr = (float *)allocator.get_host_ptr();
 
     for (size_t r = 0; r < shape.rows; r++) {
       for (size_t c = 0; c < shape.cols; c++) {
-        ptr[ptr_idx(shape.rows, r, c)] = val;
+        ptr[ptr_idx(shape.cols, r, c)] = val;
       }
     }
   }
 
   void dev_fill(float val) {
     float *ptr = (float *)allocator.get_dev_ptr();
-    // TODO
+    launch_matrix_fill_kernel(ptr, val, shape.rows, shape.cols);
+  }
+
+  void host_fill_rand(float low, float high) {
+    float *ptr = (float *)allocator.get_host_ptr();
+
+    for (size_t r = 0; r < shape.rows; r++) {
+      for (size_t c = 0; c < shape.cols; c++) {
+        float val = low + (high - low) * (float)rand() / (float)RAND_MAX;
+        ptr[ptr_idx(shape.rows, r, c)] = val;
+      }
+    }
+  }
+
+  void dev_fill_rand(float low, float high) {
+    float *ptr = (float *)allocator.get_dev_ptr();
+
+    launch_matrix_fill_rand_kernel(ptr, shape.rows, shape.cols, low, high,
+                                   rand());
+  }
+
+  void host_add(Matrix &other) {
+    assert(shape.rows == other.shape.rows);
+    assert(shape.cols == other.shape.cols);
+
+    float *self_ptr = (float *)allocator.get_host_ptr();
+    float *other_ptr = (float *)other.allocator.get_host_ptr();
+
+    for (size_t r = 0; r < shape.rows; r++) {
+      for (size_t c = 0; c < shape.cols; c++) {
+        size_t idx = ptr_idx(shape.cols, r, c);
+        self_ptr[idx] += other_ptr[idx];
+      }
+    }
+  }
+
+  void dev_add(Matrix &other) {
+    float *self_ptr = (float *)allocator.get_dev_ptr();
+    float *other_ptr = (float *)other.allocator.get_dev_ptr();
+
+    launch_matrix_add_kernel(self_ptr, other_ptr, shape.rows, shape.cols);
+  }
+
+  void host_mul(Matrix &a, Matrix &b) {
+    assert(shape.rows == a.shape.rows);
+    assert(shape.cols == b.shape.cols);
+    assert(a.shape.cols == b.shape.rows);
+
+    float *self_ptr = (float *)allocator.get_host_ptr();
+    float *a_ptr = (float *)a.allocator.get_host_ptr();
+    float *b_ptr = (float *)b.allocator.get_host_ptr();
+
+    for (size_t r = 0; r < shape.rows; r++) {
+      for (size_t c = 0; c < shape.cols; c++) {
+        float sum = 0;
+        for (size_t i = 0; i < a.shape.cols; i++) {
+          float a_val = a_ptr[ptr_idx(a.shape.cols, r, i)];
+          float b_val = b_ptr[ptr_idx(b.shape.cols, i, c)];
+          sum += a_val * b_val;
+        }
+        self_ptr[ptr_idx(shape.cols, r, c)] = sum;
+      }
+    }
+  }
+
+  void dev_mul(Matrix &a, Matrix &b) {
+    assert(shape.rows == a.shape.rows);
+    assert(shape.cols == b.shape.cols);
+    assert(a.shape.cols == b.shape.rows);
+
+    float *self_ptr = (float *)allocator.get_dev_ptr();
+    float *a_ptr = (float *)a.allocator.get_dev_ptr();
+    float *b_ptr = (float *)b.allocator.get_dev_ptr();
+
+    launch_matrix_mul_kernel(self_ptr, a_ptr, b_ptr, shape.rows, shape.cols,
+                             a.shape.cols);
   }
 };
 
