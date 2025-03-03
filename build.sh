f161b1e8d325e24a6aba2400d2a12fc27113701a
Jack Dyre <83248599+JackDyre@users.noreply.github.com>
2025-03-03 17:45:18 -0600

    .

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ export W_FLAGS=(--Werror all-warnings)
 
 set -xe
 
-nvcc -o ./result main.cu "${W_FLAGS[@]}" -arch=sm_89 -O3 --cudart shared
+nvcc -o ./result main.cu "${W_FLAGS[@]}" -arch=sm_89 -O3 --cudart static 
 
 ./result
--- main.cu
+++ main.cu
@@ -1,10 +1,14 @@
 #include "nn.cu"
 
 int main(void) {
-  // srand(time(0));
+  srand(time(0));
 
   const int input_dim = 2;
   const int output_dim = 1;
+
+  // const int epoch_count = 10000;
+
+  const float LEARNING_RATE = .01;
 
   size_t layer_sizes[] = {input_dim, 2, output_dim};
   NN nn(layer_sizes, sizeof(layer_sizes) / sizeof(size_t));
@@ -12,34 +16,63 @@ int main(void) {
 
   nn.dev_fill_rand(-1, 1);
 
-  Matrix input(Shape{.rows = input_dim, .cols = 1});
-  input.dev_fill_rand(0, 1);
-  nn.set_input_unchecked(input);
+  float inp1[2] = {0, 0};
+  float out1[1] = {0};
 
-  nn.dev_forward();
+  float inp2[2] = {1, 0};
+  float out2[1] = {1};
 
-  Matrix target_output(Shape{.rows = output_dim, .cols = 1});
-  target_output.dev_fill_rand(0, 1);
+  float inp3[2] = {0, 1};
+  float out3[1] = {1};
 
-  float cost = nn.dev_mse_cost(target_output);
-  std::cout << "Cost before training:\n" << cost << std::endl;
+  float inp4[2] = {1, 1};
+  float out4[1] = {1};
 
-  nn.print_weights();
+  Shape inp_shape = Shape{.rows = 2, .cols = 1};
+  Shape out_shape = Shape{.rows = 1, .cols = 1};
 
-  int n = 100;
-  for (int i = 0; i < n; i++) {
-    nn.set_input_unchecked(input);
+  Matrix inps[] = {Matrix::new_borrowed_host(inp1, inp_shape),
+                   Matrix::new_borrowed_host(inp2, inp_shape),
+                   Matrix::new_borrowed_host(inp3, inp_shape),
+                   Matrix::new_borrowed_host(inp4, inp_shape)};
+  Matrix outs[] = {Matrix::new_borrowed_host(out1, out_shape),
+                   Matrix::new_borrowed_host(out2, out_shape),
+                   Matrix::new_borrowed_host(out3, out_shape),
+                   Matrix::new_borrowed_host(out4, out_shape)};
+
+  float cost_before = 0;
+  for (int i = 0; i < 4; i++) {
+    nn.set_input_unchecked(inps[i]);
     nn.dev_forward();
-    nn.grad(grad_nn, target_output);
-    nn.nn_step(grad_nn, .01);
+    cost_before += nn.dev_mse_cost(outs[i]);
+  }
+  std::cout << "Cost before: " << cost_before << std::endl;
+
+  while (true) {
+    // for (int i = 0; i < epoch_count; i++) {
+    for (int k = 0; k < 4; k++) {
+      nn.set_input_unchecked(inps[k]);
+      nn.dev_forward();
+      nn.grad(grad_nn, outs[k]);
+      nn.nn_step(grad_nn, LEARNING_RATE);
+    }
+
+    float cost = 0;
+    for (int i = 0; i < 4; i++) {
+      nn.set_input_unchecked(inps[i]);
+      nn.dev_forward();
+      cost += nn.dev_mse_cost(outs[i]);
+    }
+    std::cout << "Cost: " << cost << std::endl;
   }
 
-  nn.set_input_unchecked(input);
-  nn.dev_forward();
-  float new_cost = nn.dev_mse_cost(target_output);
-  std::cout << "Cost after training:\n" << new_cost << std::endl;
-
-  nn.print_weights();
+  float cost_after = 0;
+  for (int i = 0; i < 4; i++) {
+    nn.set_input_unchecked(inps[i]);
+    nn.dev_forward();
+    cost_after += nn.dev_mse_cost(outs[i]);
+  }
+  std::cout << "Cost before: " << cost_after << std::endl;
 
   return 0;
 }
